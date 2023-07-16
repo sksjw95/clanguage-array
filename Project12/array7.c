8a21a71a5a91268fa3f681e4ce8cc77a91e645c0
sksjw95 <tjwldnd95@hanmail.net>
2023-07-16 19:59:53 +0900

    배열 요소 더하기

---
+++ Project12/array7.c
@@ -0,0 +1,11 @@
+// 배열의 각 요소에 저장된 값 합하기
+#include<stdio.h>
+void main() {
+	char data[5] = { 1,2,3,4,5 };
+	int result = 0;
+
+	for (int i = 0; i < 5; i++) {
+		result = result + data[i];
+	}
+	printf("data 배열의 각 요소의 합은 %d 입니다.", result);
+}
