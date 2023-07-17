dde11daed4d434ea7a0804a08e86061a3ec2c9ee
sksjw95 <tjwldnd95@hanmail.net>
2023-07-17 11:33:09 +0900

    문자열의 길이 구하기

---
+++ Project12/array9.c
@@ -0,0 +1,19 @@
+// 배열에 저장된 문자열의 길이를 구하는 함수 만들기
+#include<stdio.h>
+
+int GetStringLength(char data[]) {
+	int count = 0;
+	while (data[count]) {
+		count++;
+	}
+	return count;
+}
+
+void main() {
+	int data_length;
+	char data[10] = { 'h','a','p','p','y',0 };
+
+	data_length = GetStringLength(data);
+
+	printf("data length = %d\n", data_length);
+}
